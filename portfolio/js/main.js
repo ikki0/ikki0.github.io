3e54e38a6126992b68237b77bc44c9936d399937
ikki0 <arojas1297@gmail.com>
2022-02-05 19:41:51 +0100

    portflolio

---
+++ portfolio/js/main.js
@@ -0,0 +1,19 @@
+// const typed = new Typed('.Typed', {
+//     strings: ['Aprender Nuevas Cosas ', 'Trabajar en Equipo', 'Colobar ']
+// });
+
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="desarrollo">Front End Developer</i>',
+        '<i class="desarrollo">Web Designer</i>'
+    ],
+    typeSpeed: 75, //velocidad de escritura de cada letra en ms
+    startDelay: 300, //Tiempo de retraso en iniciar la animacion
+    backSpeed: 75, //velocidad para borrar una letra en ms.
+    backDelay:  1190, //Tiempo de espera tras terminar de escribir una palabra
+    loop: true, //Repite la funcion indefinidamente
+    //smartBackspace: true, elimina unicamenete aquellas palabras nuevas en el texto
+    // showCursor: true , Sirve para mostrar el cursor
+    //cursorChar: '|' , muestra dicho caracter como cursor
+    //contentType: 'html' , html o null es el tipo de texto 
+});
