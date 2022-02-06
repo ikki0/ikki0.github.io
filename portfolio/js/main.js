dd0d24f3d84281b29f843252d7069ba270d220a9
ikki0 <arojas1297@gmail.com>
2022-02-06 12:08:56 +0100

    main principal

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -4,13 +4,18 @@
 
 const typed = new Typed('.typed', {
     strings: [
-        '<i class="desarrollo">Front End Developer</i>',
-        '<i class="desarrollo">Web Designer</i>'
+        '<i class="desarrollo">Desarollar Sitios Web</i>',
+        '<i class="desarrollo">Diseñar Sitios Web</i>',
+        '<i class="desarrollo">Trabajar en Equipo</i>',
+        '<i class="desarrollo">Aprender Nuevas Cosas</i>',
+        
+        // '<i class="desarrollo">Front End Developer</i>',
+        // '<i class="desarrollo">Web Designer</i>'
     ],
     typeSpeed: 75, //velocidad de escritura de cada letra en ms
     startDelay: 300, //Tiempo de retraso en iniciar la animacion
     backSpeed: 75, //velocidad para borrar una letra en ms.
-    backDelay:  1190, //Tiempo de espera tras terminar de escribir una palabra
+    backDelay:  1185, //Tiempo de espera tras terminar de escribir una palabra
     loop: true, //Repite la funcion indefinidamente
     //smartBackspace: true, elimina unicamenete aquellas palabras nuevas en el texto
     // showCursor: true , Sirve para mostrar el cursor
